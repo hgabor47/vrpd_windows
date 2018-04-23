664f5ffecd863cdeb38a272e9ac4a181a80f134f
gabor horvath <hgabor47@gmail.com>
2018-04-23 19:49:02 +0200

    Stage 0.1

--- gitcommit.bat
+++ gitcommit.bat
@@ -3,30 +3,30 @@ echo Commit across all modules!
 SET /P message=Please enter your common commit message:
 git add .
 git commit -m "%message%"
-git push -u github master
+git push -u origin master
 
 set p0=".\bms_imagebuffer"
 echo ----------%p0%
 git -C %p0% add .
 git -C %p0% commit -m "%message%"
-git -C %p0% push -u github master
+git -C %p0% push -u origin master
 
 set p0=".\bms_inputcontroller"
 echo ----------%p0%
 git -C %p0% add .
 git -C %p0% commit -m "%message%"
-git -C %p0% push -u github master
+git -C %p0% push -u origin master
 
 set p0=".\bms_windowslist"
 echo ----------%p0%
 git -C %p0% add .
 git -C %p0% commit -m "%message%"
-git -C %p0% push -u github master
+git -C %p0% push -u origin master
 
 set p0=".\bms_screencontentexporter"
 echo ----------%p0%
 git -C %p0% add .
 git -C %p0% commit -m "%message%"
-git -C %p0% push -u github master
+git -C %p0% push -u origin master
 
 echo Finished
