96aac206340829df2d0ad8cfd3705c8f8dde4234
gabor horvath <hgabor47@gmail.com>
2018-04-23 18:20:09 +0200

    PING TEST

---
+++ githubup.bat
@@ -0,0 +1,2 @@
+git remote add github https://github.com/hgabor47/vrpd_windows.git
+git push --mirror github
--- init.bat
+++ init.bat
@@ -3,8 +3,8 @@ echo Clone all necessery BabylonMS modules?
 echo If this is an original clone from vrpd_windows then you need to load content to the empty modules' directories. If the modules' directories are not empty then you will get errors. So please start once only.
 echo If you would like to download contents (clone) then press a key or CTRL+C. 
 pause
-git clone https://gitlab.com/babylonms/bms_inputcontroller.git
-git clone https://gitlab.com/babylonms/bms_imagebuffer.git
-git clone https://gitlab.com/babylonms/bms_screencontentexporter.git
-git clone https://gitlab.com/babylonms/bms_windowslist.git
+git clone https://github.com/hgabor47/bms_inputcontroller.git
+git clone https://github.com/hgabor47/bms_imagebuffer.git
+git clone https://github.com/hgabor47/bms_screencontentexporter.git
+git clone https://github.com/hgabor47/bms_windowslist.git
 
